f62394703b02355920782aa2a446b9d4547c383d
kseniyamalkova <malkova@earthuser.ru>
2016-04-10 14:57:55 +0500

    Web server for getting list of Steam games in CSV was fixed

--- WebApp.php
+++ WebApp.php
@@ -15,13 +15,26 @@ if (php_sapi_name() === 'cli-server' && is_file($filename)) {
 
 $app = new Silex\Application();
 $app['debug'] = true;
-$file_dir = (new SteamGamesCSCreator($entityManager))->getFilePath();
+$file_path = (new SteamGamesCSVCreator($entityManager))->getFile();
+
+
+$app->get('/', function () use ($app) {
+    return "Get Steam Games in CSV: <br><a href='/GetGames'>/Games</a>";
+    });
+
+
 
 
 // /GetGames return link on csv file with data about games
-$app->get('/GetGames', function() {
-    $csv = "";
-    return $csv;
+$app->get('/GetGames', function() use ($file_path) {
+    $file = __DIR__."/". $file_path;
+    $response = new \Symfony\Component\HttpFoundation\Response();
+    $response->headers->set('Cache-Control', 'private');
+    $response->headers->set('Content-type', mime_content_type($file));
+    $response->headers->set('Content-Disposition', 'attachment; filename="' . basename($file) . '";');
+    $response->sendHeaders();
+    $response->setContent(file_get_contents($file));
+    return $response;
 });
 
 $app->run();
